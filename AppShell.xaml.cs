f7503bb93dacc2faab13402ced3076f6f3626924
arkadiuszokon <arkadiusz.okon@gmail.com>
2024-04-10 14:41:36 +0200

    Add files via upload
    ExampleProject

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ExampleTintColorApp;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace ExampleTintColorApp;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+        ListView.Header = "HeaderCell with image with tint:";
+        ListView.ItemsSource = new string[] { "lorem", "ipsum" };
+    }
+    
+}
